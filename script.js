7a37cfc1cd0c93c00838e0e1d0802e03ed97c9a4
Tej Bari <136843098+tejbari5@users.noreply.github.com>
2023-08-19 09:50:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // Your code here.
+it('should simulate click and drag to scroll', () => {
+  cy.get('.items').should('be.visible').then($items => {
+    const startPosX = 493;
+    const endPosX = 271;
+
+    cy.wrap($items)
+      .trigger('mousedown', { which: 1, pageX: startPosX, pageY: 391 })
+      .trigger('mousemove', { which: 1, pageX: endPosX, pageY: 391 })
+      .trigger('mouseup');
+
+    cy.wait(1000); // Adjust this delay as needed
+
+    cy.wrap($items).should($items => {
+      expect($items[0].scrollLeft).to.greaterThan(0);
+    });
+  });
+});
